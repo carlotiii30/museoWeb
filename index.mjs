eb83c92fbef621af54135110c780ca2c521e324a
Carlota de la Vega <carlotadlavega@gmail.com>
2025-03-05 23:18:24 +0100

    Tarea 1!

--- index.mjs
+++ index.mjs
@@ -1,26 +1,33 @@
 import express from "express";
 import nunjucks from "nunjucks";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const IN = process.env.IN || "development";
 const app = express();
 
-nunjucks.configure("views", {
+// Configurar Nunjucks
+nunjucks.configure(path.join(__dirname, "views"), {
   autoescape: true,
   noCache: IN === "development",
   watch: IN === "development",
   express: app,
 });
-app.set("view engine", "html");
+app.set("view engine", "njk");
 
-app.get("/hola", (req, res) => {
-  res.send("Hola desde el servidor");
-});
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
+// Rutas
 app.get("/", (req, res) => {
-  res.render("index.html", { saludado: "Pepito" });
+  res.render("index.njk", { titulo: "Bienvenido al Museo SSBW" });
 });
 
+// Iniciar servidor
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Servidor ejecutandose en http://localhost:${PORT} en ${IN}`);
+  console.log(
+    `Servidor ejecutándose en http://localhost:${PORT} en modo ${IN}`
+  );
 });
